58285cf07e9d36efca3759898a605ab79bfba14b
Surya Narayanan <surya23110035@snuchennai.edu.in>
2023-10-01 19:25:43 +0530

    buggy finish to top bar layout.yuck

---
+++ sketch.js
@@ -0,0 +1,45 @@
+// Side Bar
+
+
+// Logo 
+
+
+// User / guest
+
+
+// posts
+
+// async function postJSON(data) {
+//     try {
+//       const response = await fetch("https://www.snucdelta.tech/api/inductions", {
+//         mode: "no-cors",
+//         method: "POST", // or 'PUT'
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(data),
+//       });
+  
+//       const result = await response.json();
+//       console.log("Success:", result);
+//     } catch (error) {
+//       console.error("Error:", error);
+//     }
+//   }
+
+// async function getJSON() {
+//     try {
+//         const response = await fetch("https://www.snucdelta.tech/api/inductions")
+
+
+//     }
+// }
+
+fetch("https://www.snucdelta.tech/api/inductions")
+    .then(response => {
+        
+    })
+
+
+// maps
+
