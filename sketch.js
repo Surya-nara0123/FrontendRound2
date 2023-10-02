d10bc43460a251a8b73f31e0b753efd7603fa209
Surya Narayanan <surya23110035@snuchennai.edu.in>
2023-10-02 23:02:00 +0530

    displaying the geolocation and most of css is over

--- sketch.js
+++ sketch.js
@@ -1,45 +1,114 @@
-// Side Bar
+document.addEventListener("DOMContentLoaded", function () {
+    let currLoc;
+    async function getCurrentLocation() {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    resolve(position);
+                },
+                (error) => {
+                    reject(error);
+                }
+            );
+        });
+    }
 
+    async function main() {
+        try {
+            const position = await getCurrentLocation();
+            const { latitude, longitude } = position.coords;
+            // Show a map centered at latitude / longitude.
+            currLoc = [latitude, longitude];
+            console.log(currLoc);
+            const posts = [
+                {
+                    "id": "1",
+                    "title": "New Post Title",
+                    "content": "\t\t\tNew Post \tContent Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content ",
+                    "createdLat": 123.456,
+                    "createdLong": 789.012
+                },
+                {
+                    "id": "2",
+                    "title": "Post Title 2",
+                    "content": "Post Content 2",
+                    "createdLat": 456.789,
+                    "createdLong": 12.345
+                }
+            ];
 
-// Logo 
+            const postsElement = document.querySelector(".posts");
 
+            for (const post of posts) {
+                const postElement = document.createElement("div");
+                postElement.className = "post"
+                const heading = document.createElement("div")
+                heading.className = "heading"
+                const title = document.createElement("div");
+                title.className = "title"
+                const distance = document.createElement("div");
+                distance.className = "distance"
+                const contents = document.createElement("div");
+                contents.className = "contents"
 
-// User / guest
+                title.innerHTML = post["title"];
 
+                const latDiff = post["createdLat"] - currLoc[0];
+                const longDiff = post["createdLong"] - currLoc[1];
+                const distanceValue = Math.sqrt(latDiff * latDiff + longDiff * longDiff);
+                distance.innerHTML = distanceValue.toFixed(2);
 
-// posts
+                contents.innerHTML = "<br>" + post["content"];
 
-// async function postJSON(data) {
-//     try {
-//       const response = await fetch("https://www.snucdelta.tech/api/inductions", {
-//         mode: "no-cors",
-//         method: "POST", // or 'PUT'
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(data),
-//       });
-  
-//       const result = await response.json();
-//       console.log("Success:", result);
-//     } catch (error) {
-//       console.error("Error:", error);
-//     }
-//   }
+                heading.appendChild(title);
+                heading.appendChild(distance);
+                postElement.appendChild(heading);
+                postElement.appendChild(contents);
 
-// async function getJSON() {
-//     try {
-//         const response = await fetch("https://www.snucdelta.tech/api/inductions")
+                postsElement.appendChild(postElement);
+            }
+        } catch (error) {
+            console.error("Error getting location:", error);
+        }
+    }
+    main()
 
+});
 
-//     }
-// }
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 6,
+    });
+    infoWindow = new google.maps.InfoWindow();
 
-fetch("https://www.snucdelta.tech/api/inductions")
-    .then(response => {
-        
-    })
+    const locationButton = document.createElement("button");
 
+    locationButton.textContent = "Pan to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
 
-// maps
-
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent("Location found.");
+                    infoWindow.open(map);
+                    map.setCenter(pos);
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                },
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    });
+}
